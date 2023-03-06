693e07df01877afaf7a013e737bda9d67c86b0c1
Emika Kiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-03-06 12:56:18 +0000

    [管理者ログイン機能、マイページ編集、ゲストログイン機能を追加しました]

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,23 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+# プロフィール画像の設定
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.png')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/jpeg' )
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  # ゲストログインの設定
+  def self.guest
+    find_or_create_by!(name: 'guestmember', email: 'guest1@example.com') do |member|
+      member.password = SecureRandom.urlsafe_base64
+      member.name = "guestmember"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,49 @@
 Rails.application.routes.draw do
 
-  # 管理者用URL
+  # 管理者側
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     # サインアップ、パスワードの変更を排除
   sessions: "admin/sessions"
 }
 
-  # 顧客用URL
+  # 顧客側
   devise_for :members, skip: [:passwords], controllers: {
     # パスワードの変更を排除
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  # ゲストメンバーでのサインイン
+  devise_scope :member do
+    post 'members/guest_sign_in', to: 'members/sessions#guest_sign_in'
+  end
 
   namespace :admin do
     # 管理者トップページ(会員一覧・会員退会機能)
     get '/' => 'members#index'
     resources :members, only: [:update]
     # ↑の記述があってるかわからない。トップ画面のアクションとして成立するのか確認する。またリーダブルコードとして修正するならどうするのが良いかも確認。
-    
+
     # 管理者側検索画面
     get 'searches' => 'searches#search', as:'search'
-    
+
     # 管理者側投稿機能に、コメント機能をネストさせています
     resources :posts, only: [:index, :show, :destroy] do
       resources :post_comments, only: [:destroy]
     end
   end
-  
+
   scope module: :public do
     root to: "homes#top"
-    
+
     # 顧客側検索機能画面
     get 'searches' => 'searches#search', as:'search'
-    
+
     # 顧客側の投稿機能に、応援、コメント機能をネストさせています
     resources :posts, only: [:new, :create, :show, :index, :edit, :update, :destroy] do
       resources :yells, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
     end
-    
+
     # 顧客側のマイページ
     resources :members, only: [:show, :edit, :update, :destroy]
   end
