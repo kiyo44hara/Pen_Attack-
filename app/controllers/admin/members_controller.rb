02e26af45f14059bbb8eb544008421e1a42b0f37
Emika Kiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-03-06 08:52:11 +0000

    [ルーティングの記述が終了しました]

---
+++ app/controllers/admin/members_controller.rb
@@ -0,0 +1,9 @@
+class Admin::MembersController < ApplicationController
+  
+  def index
+  end
+  
+  def update
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  
- Rails.application.routes.draw do
 
   # 管理者用URL
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
@@ -16,9 +14,34 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-    get '/' => 'homes#top'
-
+    # 管理者トップページ(会員一覧・会員退会機能)
+    get '/' => 'members#index'
+    resources :members, only: [:update]
+    # ↑の記述があってるかわからない。トップ画面のアクションとして成立するのか確認する。またリーダブルコードとして修正するならどうするのが良いかも確認。
+    
+    # 管理者側検索画面
+    get 'searches' => 'searches#search', as:'search'
+    
+    # 管理者側投稿機能に、コメント機能をネストさせています
+    resources :posts, only: [:index, :show, :destroy] do
+      resources :post_comments, only: [:destroy]
+    end
   end
-end
+  
+  scope module: :public do
+    root to: "homes#top"
+    
+    # 顧客側検索機能画面
+    get 'searches' => 'searches#search', as:'search'
+    
+    # 顧客側の投稿機能に、応援、コメント機能をネストさせています
+    resources :posts, only: [:new, :create, :show, :index, :edit, :update, :destroy] do
+      resources :yells, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    
+    # 顧客側のマイページ
+    resources :members, only: [:show, :edit, :update, :destroy]
+  end
 
 end
