495f2018e4948700b1652f3ead5bd9e4ec35b4fc
Emika Kiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-03-07 06:25:46 +0000

    [管理者機能全般追加しました]

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,9 +1,22 @@
 class Admin::MembersController < ApplicationController
-  
+    before_action :authenticate_admin!
+
   def index
+    @members = Member.all
   end
-  
+
+  # 会員のステータス更新。
+  # また、ApplicationControllerにて、管理者権限と、会員本人以外はステータスの更新ができないようにしています。
   def update
+    @member = Member.find(params[:id])
+    if @member.is_deleted == false
+      @member.update(is_deleted: true)
+      redirect_to admin_path, notice: '会員を退会させました'
+    elsif @member.is_deleted == true
+      @member.update(is_deleted: false)
+      redirect_to admin_path, notice: '会員を復活させました'
+    else
+      reder 'index'
+    end
   end
-  
 end
