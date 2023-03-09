da1ffdc3f9dc65ff3f5ecf74891ca682d3c84e10
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-03-09 05:27:32 +0000

    [新規投稿機能を実装しました]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,9 +1,13 @@
 class Public::PostsController < ApplicationController
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to member_path(current_member)
   end
 
   def show
@@ -19,5 +23,10 @@ class Public::PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :appeal_point, :improve_point, :production_time, :star)
   end
 end
