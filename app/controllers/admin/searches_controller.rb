39d6f5fc94c30e49354ef3373b8e274ab3bee38f
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-03-26 08:04:23 +0000

    [管理者側の検索機能の不具合尾を修正しました]

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,26 +1,29 @@
 class Admin::SearchesController < ApplicationController
   before_action :authenticate_admin!
 
+  # 検索内容に応じて、表示する順番も指定しています。
   def search
     @range = params[:range]
     @word = params[:word]
+    @search = params[:search]
     if @range == "クリエイター"
         @members = Member.looks(params[:search], params[:word]).page(params[:page])
     elsif @range == "イラスト"
+      @posts = Post.looks(params[:search], params[:word]).latest.page(params[:page])
       if params[:old]
-        @posts = Post.looks(params[:search], params[:word]).old.page(params[:page])
+         @posts = Post.looks(params[:search], params[:word]).old.page(params[:page])
       else
         @posts = Post.looks(params[:search], params[:word]).latest.page(params[:page])
       end
     else
-      @tag = Tag.looks(params[:search], params[:word])
-      # もし該当するタグがなくても、画面が表示されるように記述
-      if !@tag.present?
-        @posts = Kaminari.paginate_array(["あいうえお"]).page(params[:page])
-      elsif params[:old]
-        @posts = Kaminari.paginate_array@posts.page(params[:page]).old
+      tags = Tag.looks(params[:search], params[:word])
+      # タグに紐づいた投稿を取り出す(flatten：配列を整える。uniq：レコードの重複を防ぐ)
+      post_ids = tags.map { |tag| tag.posts.pluck(:id) }.flatten.uniq
+      @posts = Post.where(id: post_ids).latest.page(params[:page])
+      if params[:old]
+        @posts = Post.where(id: post_ids).old.page(params[:page])
       else
-        @posts = @posts.page(params[:page]).latest
+        @posts = Post.where(id: post_ids).latest.page(params[:page])
       end
     end
   end
