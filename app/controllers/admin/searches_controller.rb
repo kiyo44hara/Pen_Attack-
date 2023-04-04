1d3fc6c7289fafeefe64abfbf570810792e1bd4f
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-04-04 13:13:17 +0000

    [検索機能のリファクタリングを行いました]

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,30 +1,28 @@
 class Admin::SearchesController < ApplicationController
   before_action :authenticate_admin!
 
-  # 検索内容に応じて、表示する順番も指定しています。
+  # 検索内容に応じて、表示する順番も指定しています(昇順・降順)。
   def search
     @range = params[:range]
     @word = params[:word]
     @search = params[:search]
+
+  # メンバー検索
     if @range == "クリエイター"
         @members = Member.looks(params[:search], params[:word]).page(params[:page]).per(15)
+  # 投稿検索
     elsif @range == "イラスト"
-      @posts = Post.looks(params[:search], params[:word]).latest.page(params[:page])
-      if params[:old]
-         @posts = Post.looks(params[:search], params[:word]).old.page(params[:page])
-      else
-        @posts = Post.looks(params[:search], params[:word]).latest.page(params[:page])
-      end
+      @posts = Post.looks(params[:search], params[:word]).latest
+      @posts = Post.looks(params[:search], params[:word]).old if params[:old]
+      @posts = @posts.page(params[:page])
+  # タグ検索
     else
       tags = Tag.looks(params[:search], params[:word])
-      # タグに紐づいた投稿を取り出す(flatten：配列を整える。uniq：レコードの重複を防ぐ)
+  # タグに紐づいた投稿を取り出す(flatten：配列を整える。uniq：レコードの重複を防ぐ)
       post_ids = tags.map { |tag| tag.posts.pluck(:id) }.flatten.uniq
-      @posts = Post.where(id: post_ids).latest.page(params[:page])
-      if params[:old]
-        @posts = Post.where(id: post_ids).old.page(params[:page])
-      else
-        @posts = Post.where(id: post_ids).latest.page(params[:page])
-      end
+      @posts = Post.where(id: post_ids).latest
+      @posts = Post.where(id: post_ids).old if params[:old]
+      @posts = @posts.page(params[:page])
     end
   end
 end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,33 +1,28 @@
 class Public::SearchesController < ApplicationController
   before_action :authenticate_member!
 
-  # 検索内容に応じて、表示する順番も指定しています。
+  # 検索内容に応じて、表示する順番も指定しています(昇順・降順)。
   def search
     @range = params[:range]
     @word = params[:word]
     @search = params[:search]
-    # クリエイター＝メンバー検索
+
+  # メンバー検索
     if @range == "クリエイター"
         @members = Member.looks(params[:search], params[:word]).page(params[:page]).per(15)
+  # 投稿検索
     elsif @range == "イラスト"
       @posts = Post.looks(params[:search], params[:word]).latest
-      if params[:old]
-         @posts = Post.looks(params[:search], params[:word]).old
-      else
-        @posts = Post.looks(params[:search], params[:word]).latest
-      end
-        @posts = @posts.page(params[:page])
+      @posts = Post.looks(params[:search], params[:word]).old if params[:old]
+      @posts = @posts.page(params[:page])
+  # タグ検索
     else
       tags = Tag.looks(params[:search], params[:word])
-      # タグに紐づいた投稿を取り出す(flatten：配列を整える。uniq：レコードの重複を防ぐ)
+  # タグに紐づいた投稿を取り出す(flatten：配列を整える。uniq：レコードの重複を防ぐ)
       post_ids = tags.map { |tag| tag.posts.pluck(:id) }.flatten.uniq
       @posts = Post.where(id: post_ids).latest
-      if params[:old]
-        @posts = Post.where(id: post_ids).old
-      else
-        @posts = Post.where(id: post_ids).latest
-      end
-        @posts = @posts.page(params[:page])
+      @posts = Post.where(id: post_ids).old if params[:old]
+      @posts = @posts.page(params[:page])
     end
   end
 end
