8fe8fc42e2ce60a22699bdc50f71cca8538f9b96
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-04-16 06:15:14 +0000

    [誤字の修正を行いました]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-   config.action_mailer.raise_delivery_errors = ture
+   config.action_mailer.raise_delivery_errors = true
    mail = ENV["ACTION_MAILER_ADMIN"]
    key = ENV["ACTION_MAILER_KEY"]
 
